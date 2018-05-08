2c6491c71f084b4a7f37edbef73d34b4d4649ee3
Juan Bautista <juanbautista19@gmail.com>
2018-05-08 13:11:02 +0200

    Mejorado/Corregido combineReducers
    Mejorado/Corregido combineReducers

--- src/store.js
+++ src/store.js
@@ -1,11 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-const products = (state, action) => {
-
-    if(state === undefined) {
-        return [];
-    }
+const products = (state=[], action) => {
 
     if(action.type === 'REPLACE_PRODUCTS') {
         return action.products;
@@ -14,18 +10,14 @@ const products = (state, action) => {
 };
 
 
-const cart = (state, action) => {
-
-    if(state === undefined) {
-        return [];
-    }
+const cart = (state=[], action) => {
 
     if(action.type === "ADD_TO_CART") {
-        return action.product;
+        return state.concat(action.product);
 
     } else if(action.type === 'REMOVE_FROM_CART') {
         let productIndex = action.product.id;
-        return state.cart.filter((product) => {
+        return state.filter((product) => {
                 return product.id !== productIndex
             });
     }
